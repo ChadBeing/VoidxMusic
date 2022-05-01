201cff8bdd249b97fcec0045cc66a13e4405bef5
Husbando <104649844+ChadBeing@users.noreply.github.com>
2022-05-01 21:23:26 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -28,8 +28,8 @@ from Void X Music.utils.database import (add_served_chat,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from YukkiMusic.utils.decorators.language import LanguageStart
-from YukkiMusic.utils.inline import (help_pannel, private_panel,
+from Void X Music.utils.decorators.language import LanguageStart
+from Void X Music.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
 loop = asyncio.get_running_loop()
@@ -114,7 +114,7 @@ async def start_comm(client, message: Message, _):
                 sender_id = message.from_user.id
                 sender_name = message.from_user.first_name
                 return await app.send_message(
-                    config.LOG_GROUP_ID,
+                    config." TeamNexusX ",
                     f"{message.from_user.mention} has just started bot to check <code>SUDOLIST</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
             return
@@ -192,7 +192,7 @@ async def start_comm(client, message: Message, _):
             OWNER = OWNER_ID[1938491135]
         except:
             OWNER = "Husbandoo"
-        out = private_panel(_, app.Username, OWNER)
+        out = private_panel(_, app."Husbandoo")
         if config.START_IMG_URL:
             try:
                 await message.reply_photo(
